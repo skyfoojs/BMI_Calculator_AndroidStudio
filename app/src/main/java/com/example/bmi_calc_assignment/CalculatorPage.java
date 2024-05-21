31acab4b07dbcc5bdc957d657fe691e2b39e684e
skyfoojs <skyfoojs@gmail.com>
2024-05-21 15:29:30 +0800

    UI Design is almost done, but need to find out the plus and minus sign should use ID or onClick

--- app/src/main/java/com/example/bmi_calc_assignment/CalculatorPage.java
+++ app/src/main/java/com/example/bmi_calc_assignment/CalculatorPage.java
@@ -1,25 +1,37 @@
 package com.example.bmi_calc_assignment;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.slider.Slider;
+
+import java.text.DecimalFormat;
+
 public class CalculatorPage extends AppCompatActivity {
 
+    TextView sliderAgeText;
+    Slider slider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calculator_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        slider = findViewById(R.id.slider);
+        sliderAgeText = findViewById(R.id.sliderHeightText);
+
+        slider.addOnChangeListener(new Slider.OnChangeListener() {
+            @Override
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                DecimalFormat df = new DecimalFormat("###.#");
+                sliderAgeText.setText(df.format(value));
+            }
+        });
     }
 }
