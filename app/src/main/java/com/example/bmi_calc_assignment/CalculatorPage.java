5e2766922845f7e58bf61b2a4231357b896627d4
skyfoojs <skyfoojs@gmail.com>
2024-05-27 18:47:21 +0800

    Changed color for selecting gender

--- app/src/main/java/com/example/bmi_calc_assignment/CalculatorPage.java
+++ app/src/main/java/com/example/bmi_calc_assignment/CalculatorPage.java
@@ -1,14 +1,11 @@
 package com.example.bmi_calc_assignment;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
